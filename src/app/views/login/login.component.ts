489ced4c61c3f984a639877d69cbb98e73958e4f
Marcio Rodrigues <bodybuildermarcio@gmail.com>
2020-11-20 00:03:27 -0300

    Alterações login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CreateuserService } from '../createuser/createuser.service';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +12,19 @@ export class LoginComponent implements OnInit {
   email : string = '';
   password: string = '';
     
-  constructor() { }
+  constructor(private login: CreateuserService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  loginUser(): void {
+
+    this.login.login(this.email, this.password).subscribe(() => {
+      this.login.showMessage('operação executada com sucesso');
+      this.router.navigate(['/products']);
+    })
+
   }
 
 }
